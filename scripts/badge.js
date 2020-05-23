94e8fc6cb8ad48a3ac5dbb352d9f1572671f03ea
Yahya Gilany <yahya.gilany@live.com>
2020-05-23 17:17:21 -0400

    script update

--- scripts/badge.js
+++ scripts/badge.js
@@ -11,9 +11,13 @@ async function main(){
     file: `README.md`,
     line: 3,
     text: result,
-    callback: ({ file, line, text, replacedText, error }) => {
-      process.exit(0);
-    }
+    callback: ({file, line, text, replacedText, error}) => {}
+  });
+  lineReplace({
+    file: `README.md`,
+    line: 8,
+    text: `- [x] update the assignment checks above to the correct link. - Done Automatically`,
+    callback: ({file, line, text, replacedText, error}) => {}
   });
 }
 
