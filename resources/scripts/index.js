1a027c4d6c431089f26a7cf2f729044d04112c78
Yahya Gilany <yahya.gilany@live.com>
2020-05-23 16:47:42 -0400

    push

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,3 +1,28 @@
-const game = new RockPaperScissors();
-game.print();
+// Elements
+const welcomeScreen = document.getElementById(`welcome-screen`);
+const gameScreen = ;
+const startGameButton = ;
+const userName = ;
+const userSelection = ;
+const goButton = ;
+const scoreParagraph = ;
+const gameHistoryParagraph = ;
 
+// instantiate the game object from the `RockPaperScissors` class.
+const game = new RockPaperScissors();
+// Placeholder variables
+
+// hide gamescreen
+gameScreen.classList.add(`d-none`);
+
+function updateScoreTally(){
+
+}
+
+function updateGameHistory(){
+
+}
+
+// start-game-button EventListener
+
+// go-button EventListener
